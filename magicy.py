9f7ca6f39360db0fe5054fb8d2e142134c7e8793
MDomanski-dev <63311397+MDomanski-dev@users.noreply.github.com>
2020-04-07 20:08:08 +0200

    Delete magicy.py

--- magicy.py
+++
@@ -1,7 +0,0 @@
-
-magicians = ['maja', 'majas', 'majak']
-for magician in magicians:
-    print(magician.title() + ", Super sztuczka! ")
-    print("Rozkaz, nie przepuszczać nikogo! " + magician.title() + ".\n") 
-    
-print("Było miło, dzięki")    
