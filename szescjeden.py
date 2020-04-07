1b832f5b268d4f7d75016dfbc21e10d46c6c0dd4
MDomanski-dev <63311397+MDomanski-dev@users.noreply.github.com>
2020-04-07 20:10:35 +0200

    Delete szescjeden.py

--- szescjeden.py
+++
@@ -1,6 +0,0 @@
-osoba = {'first_name': 'Marcin','last_name':'Domański','Age':'26','city':'Swarzędz'}
-
-print(osoba['first_name'])
-print(osoba['last_name'])
-print(osoba['Age'])
-print(osoba['city'])
